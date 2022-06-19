6129bfb8731960c3876984eef0f018609ab4a042
ashuthosh-bharadwaj <ashuthosh.bharadwaj@research.iiit.ac.in>
2022-06-20 01:18:20 +0530

    buncha changes

--- Simulation2/mat_adj.py
+++ Simulation2/mat_adj.py
@@ -1,4 +1,13 @@
+import numpy as np
+import pandas as pd
+import pickle as pkl
+
 def Adj(N):
-    A_gt = abs(rand(N,N))
+    A_gt = np.abs(np.random.randn(N,N))
     for i in range(N): A_gt[i,i] = 0
     A_gt = (A_gt + A_gt.T)/2
+    return A_gt
+
+def file(N):
+    pkl.dump(Adj(N), open("Adj.pkl", "wb"))
+    return "./Adj.pkl"
