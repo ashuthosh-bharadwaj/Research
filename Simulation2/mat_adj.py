d8210e79cd5faf922b0d7659be4755f873b6ccf2
ashuthosh-bharadwaj <ashuthosh.bharadwaj@research.iiit.ac.in>
2022-06-20 14:28:03 +0530

    yet

--- Simulation2/mat_adj.py
+++ Simulation2/mat_adj.py
@@ -11,3 +11,23 @@ def Adj(N):
 def file(N):
     pkl.dump(Adj(N), open("Adj.pkl", "wb"))
     return "./Adj.pkl"
+
+
+def U_synth(u):
+    N = u.shape[0]
+    for i in range(1,N):
+        if i == 1:
+            uL = u[i:,:].T
+            I = u[i-1,:]*np.eye(N-i)
+            t = np.append(uL,I,axis=0)
+            U = t
+        
+        else:
+            Z = np.zeros((i-1,N-i))
+            uL = u[i:,:].T
+            I = u[i-1,:]*np.eye(N-i)
+            t = np.append(Z,uL,axis=0)
+            t = np.append(t,I,axis=0)    
+            U = np.append(U,t, axis=1)
+    return U
+
