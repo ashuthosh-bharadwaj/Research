3034f96a075387105be29be72eb857b7c957c409
ashuthosh-bharadwaj <ashuthosh.bharadwaj@research.iiit.ac.in>
2022-06-20 14:36:27 +0530

    Again why wont this work

--- Simulation3/Make_Lap.py
+++ Simulation3/Make_Lap.py
@@ -1,20 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle as pkl
-
-def Make(N):
-    A_gt = np.abs(np.random.randn(N,N))
-    for i in range(N): A_gt[i,i] = 0
-    A_gt = (A_gt + A_gt.T)/2
-    D = np.diag(np.dot(A_gt, np.ones(N,)))
-    return D, A_gt
-
-def Store(N):
-    d, a = Make(N)
-    pkl.dump(d, open("Deg.pkl", "wb"))
-    pkl.dump(a, open("Adj.pkl", "wb"))
-    return "./Deg.pkl" , "./Adj.pkl"
-    
+ 
 def U_synth(u):
     N = u.shape[0]
     for i in range(1,N):
@@ -32,3 +19,16 @@ def U_synth(u):
             t = np.append(t,I,axis=0)    
             U = np.append(U,t, axis=1)
     return U
+
+def Make(N):
+    A_gt = np.abs(np.random.randn(N,N))
+    for i in range(N): A_gt[i,i] = 0
+    A_gt = (A_gt + A_gt.T)/2
+    D = np.diag(np.dot(A_gt, np.ones(N,)))
+    return D, A_gt
+
+def Store(N):
+    d, a = Make(N)
+    pkl.dump(d, open("Deg.pkl", "wb"))
+    pkl.dump(a, open("Adj.pkl", "wb"))
+    return "./Deg.pkl" , "./Adj.pkl"
